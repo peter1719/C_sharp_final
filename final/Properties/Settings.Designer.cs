78a90acac4e79b3ecd5cbd961aaf87b5d5188f1e
peter <peterpenno1@gmail.com>
2019-12-31 15:20:47 +0800

    Add the database and the relation

--- final/Properties/Settings.Designer.cs
+++ final/Properties/Settings.Designer.cs
@@ -22,5 +22,16 @@ namespace final.Properties {
                 return defaultInstance;
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\theQuest.mdf;" +
+            "Integrated Security=True")]
+        public string theQuestConnectionString {
+            get {
+                return ((string)(this["theQuestConnectionString"]));
+            }
+        }
     }
 }
